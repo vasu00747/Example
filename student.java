59d575709da7f358739447ee770de02e14a6b4a5
vasu00747 <vasamalli47@gmail.com>
2019-05-19 12:44:32 +0530

    Added files

---
+++ student.java
@@ -0,0 +1,12 @@
+public class student{
+int a=10;
+public void display()
+{
+System.out.println("Asdf agjushfjs");
+}
+public static void main(String args[])
+{
+student s=new student();
+s.display();
+}
+}
